dbfed6697919032612b03fc1b6c9cc8529554780
Sandeep Neupane <deep.newsun@gmail.com>
2020-04-08 01:24:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import * as WebDataRocksReact from "./webdatarocks.react";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <WebDataRocksReact.Pivot
+          toolbar={true}
+          report="https://jsonplaceholder.typicode.com/posts/1/comments"
+          // report="https://cdn.webdatarocks.com/reports/report.json"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
